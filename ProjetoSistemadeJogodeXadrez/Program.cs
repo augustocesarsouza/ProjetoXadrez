322251ae52c8b46e77b2517ab7111527d84c9fb3
augustocesarsouza <augustocesarsantana90@gmail.com>
2023-03-07 08:59:47 -0400

    Criando a classe PosicaoXadrez

--- ProjetoSistemadeJogodeXadrez/Program.cs
+++ ProjetoSistemadeJogodeXadrez/Program.cs
@@ -10,20 +10,11 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez(7, 'c');
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+            Console.WriteLine(pos);
 
-                Tela.ImprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine($"Erro Tabuleiro: {e.Message}");
-            }
+            Console.WriteLine(pos.ToPosicao());
 
             Console.ReadLine();
         }
---
+++ ProjetoSistemadeJogodeXadrez/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+﻿using tabuleiro;
+namespace xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public int Linha { get; set;}
+        public char Coluna { get; set; } //No xadrez a Coluna "AS LETRAS" e as Linha "Os numeros"
+
+        public PosicaoXadrez(int linha, char coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+            
+        }
+
+        public Posicao ToPosicao() //Para conver uma posição normal 
+        {
+            return new Posicao(8 - Linha, Coluna - 'a'); //Aqui ele ta usando os codigo da "tabela ASCII" no caso 'a'=97 se eu quiser Coluna 'd'=100 entao 97-100=3
+        }
+
+        public override string ToString()
+        {
+            return $"{Coluna}{Linha}";
+        }
+    }
+}
